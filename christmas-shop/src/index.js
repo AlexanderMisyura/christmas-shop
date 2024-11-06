825bdfbb7394005f5158281b04d1f8349468861c
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-11-07 00:14:10 +0300

    feat: add scroll up button

--- christmas-shop/src/index.js
+++ christmas-shop/src/index.js
@@ -2,6 +2,8 @@ import './main.scss';
 
 import fillGiftsSection from './scripts/fillGiftsSection';
 import toggleSidebar from './scripts/processSidebar';
+import scrollBtn from './scripts/scrollBtn';
 
 fillGiftsSection();
 toggleSidebar();
+scrollBtn();
---
+++ christmas-shop/src/scripts/scrollBtn.js
@@ -0,0 +1,15 @@
+export default function scrollBtn() {
+  const btn = document.querySelector('.btn_up');
+
+  btn.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+      btn.classList.add('btn_up_disabled');
+    } else if (btn.classList.contains('btn_up_disabled')) {
+      btn.classList.remove('btn_up_disabled');
+    }
+  });
+}
