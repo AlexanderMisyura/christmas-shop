d859f61c46c9d99efb81757a43a92ee3229241b9
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-11-08 15:39:09 +0300

    feat: add timer functionality

--- christmas-shop/src/index.js
+++ christmas-shop/src/index.js
@@ -4,6 +4,7 @@ import fillGiftsSection from './scripts/fillGiftsSection';
 import toggleSidebar from './scripts/processSidebar';
 import scrollBtn from './scripts/scrollBtn';
 import Slider from './scripts/slider';
+import Timer from './scripts/timer';
 
 fillGiftsSection();
 toggleSidebar();
@@ -11,3 +12,6 @@ scrollBtn();
 
 const slider = new Slider(document.querySelector('.slider-row'));
 slider.listen();
+
+const timer = new Timer(document.querySelector('.timer'));
+timer.run();
