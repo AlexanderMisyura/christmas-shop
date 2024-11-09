ff5d0859b4f35d75fc98177d99fb5010b732d78a
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-11-09 20:53:12 +0300

    feat: add preloader

--- christmas-shop/src/index.js
+++ christmas-shop/src/index.js
@@ -1,11 +1,13 @@
 import './main.scss';
 
+import preloader from './scripts/preloader';
 import fillGiftsSection from './scripts/fillGiftsSection';
 import toggleSidebar from './scripts/processSidebar';
 import scrollBtn from './scripts/scrollBtn';
 import Slider from './scripts/slider';
 import Timer from './scripts/timer';
 
+preloader();
 fillGiftsSection();
 toggleSidebar();
 scrollBtn();
