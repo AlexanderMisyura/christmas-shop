1b56b0ae0458f85749a0e0d5de7bc2da701b6b2e
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-12-11 15:10:22 +0300

    feat: add class for component creation and tag generator

--- christmas-shop/src/app/types.js
+++ christmas-shop/src/app/types.js
@@ -1,4 +1,48 @@
 /**
+ * @typedef {Object} DataAttribute - An array of objects representing data attributes.
+ * @property {string} key - The name of the data attribute (with no "data-" prefix).
+ * @property {string} value - The value of the data attribute.
+ */
+
+/**
+ * @typedef {Extract<keyof HTMLElementTagNameMap, 'header' | 'div' | 'a' | 'img' | 'span' | 'nav' | 'ul' | 'li' | 'button' | 'main' | 'section' | 'h1' | 'footer' | 'h2' | 'p'>} TagType - The values of HTML tags (such as 'p', 'span', etc.)
+ */
+
+/**
+ * @typedef {Object<string, Function>} PropsCallbacks
+ */
+
+/**
+ * @typedef {Object} Props - Properties for component generation.
+ * @property {keyof HTMLElementTagNameMap} [elementTagName] - Element's HTML tag.
+ * @property {string} [text] - Element's text content (optional).
+ * @property {string[] | string } [classList] - An array of CSS class names or a single CSS class string (optional).
+ * @property {DataAttribute[]} [dataset] - Custom data attributes (optional).
+ * @property {string} [style] - CSS styles as a string (optional).
+ * @property {string} [id] - Unique id (optional).
+ * @property {string} [href] - The URL the link goes to (for 'a' tag).
+ * @property {string} [target] - Specifies where to open the linked document (for 'a' tag).
+ * @property {string} [alt] - Alternative text for the image (for 'img' tag).
+ * @property {string} [src] - A path to the source file (for 'img' tag).
+ * @property {string} [width] - The width of the image (for 'img' tag).
+ * @property {string} [height] - The height of the image (for 'img' tag).
+ * @property {string} [type] - The type of the button (for 'button' tag).
+ * @property {PropsCallbacks} [callbacks] - An object containing callbacks to pass through components.
+ */
+
+/**
+ * @typedef {{
+ *   name: string,
+ *   description: string,
+ *   superpowers: {
+ *     live: string,
+ *     create: string,
+ *     love: string,
+ *     dream: string
+ *   }
+ * }} Gift
+
+/**
  * An object structure to be consumed by router loader.
  * @typedef {Object} RouteObj
  * @property {string} pathname
@@ -6,11 +50,19 @@
  */
 
 /**
+ * @typedef {import('./state').default} State
+ */
+
+/**
+ * @typedef {import('./router').default} Router
+ */
+
+/**
  * @typedef {Object} StateObj
  * @property {Boolean} isSidebarOpen
  * @property { 3 | 6 } sliderSteps
  * @property {'/' | '/gifts'} currentPage
- * @property {Object[]} gifts
+ * @property {Gift[]} gifts
  */
 
 /**
