2f2a9b8db554ead5cab2e77081b4578c66cafc83
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-12-13 11:43:18 +0300

    feat: add page switch functionality

--- christmas-shop/src/app/components/page.js
+++ christmas-shop/src/app/components/page.js
@@ -14,26 +14,89 @@ const TABLET_BREAKPOINT = `(width <= ${TABLET_WIDTH}px)`;
 const SLIDER_STEPS_NARROW_SCREEN = 6;
 
 export default class Page extends BaseComponent {
+  // #region components
+  /** @type {HTMLElement} */
+  main;
+
+  /** @type {BaseComponent} */
+  homePage;
+
+  /** @type {Header} */
+  header;
+
+  /** @type {Sidebar} */
+  sidebar;
+
+  /** @type {Hero} */
+  hero;
+
+  /** @type {About} */
+  about;
+
+  /** @type {Slider} */
+  slider;
+  // #endregion
+
   /**
-   * @param {State} [state]
-   * @param {Props} [props]
+   * @param {Props} props
+   * @param {State} state
    */
   constructor(props, state) {
-    super(
-      { elementTagName: 'div', classList: 'page' },
-      [
-        new Header(props, state),
-        new Sidebar(props, state),
-        new BaseComponent(
-          { elementTagName: 'main', classList: 'main' },
-          [new Hero(props, state), new About(), new Slider(props, state)],
-          state
-        ),
-      ],
-      state
+    super({ elementTagName: 'div', classList: 'page' }, [], state);
+
+    this.header = new Header(props, state);
+    this.sidebar = new Sidebar(props, state);
+    this.hero = new Hero(props);
+    this.about = new About();
+    this.slider = new Slider(state);
+
+    this.homePage = new BaseComponent(
+      { elementTagName: 'main', classList: 'main' },
+      [this.hero, this.about, this.slider]
+    );
+    this.giftsPage = new BaseComponent(
+      { elementTagName: 'main', classList: 'main' },
+      []
     );
 
+    this.main = this.homePage.element;
+
+    this.appendChildElements([this.header, this.sidebar, this.homePage]);
+
     this.listen();
+
+    if (this.state) {
+      this.currentState = { currentPage: this.state.stateObj.currentPage };
+      this.subscribe(this.state, this.updatePage.bind(this));
+    }
+  }
+
+  /**
+   * A method to be called by publisher: updates the content of the page.
+   * @param {StateObj} stateObj
+   * @returns {void}
+   */
+  updatePage(stateObj) {
+    if (this.currentState.currentPage === stateObj.currentPage) return;
+
+    switch (stateObj.currentPage) {
+      case '/':
+        this.replaceCurrentPage(this.homePage.element);
+        break;
+
+      case '/gifts':
+        this.replaceCurrentPage(this.giftsPage.element);
+        break;
+
+      default:
+        break;
+    }
+    this.currentState.currentPage = stateObj.currentPage;
+  }
+
+  replaceCurrentPage(newPageElement) {
+    this.main.replaceWith(newPageElement);
+    this.main = newPageElement;
   }
 
   mount() {
